f9e5dc36c3dc5834aee8d57a798b704c9c1d9248
Kaway <506158374@qq.com>
2017-03-08 12:03:27 +0800

    1.项目创建

---
+++ newsListDemo/newsListDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newsListDemo
+//
+//  Created by 梁家伟 on 17/3/8.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
